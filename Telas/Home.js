7dce5d0b0cd61ed8ed6f39e3ec1055aefb67ac6e
UltraFire66 <iagorg2000@gmail.com>
2025-10-20 16:02:24 -0300

    tela de home feita

--- Telas/Home.js
+++ Telas/Home.js
@@ -1,7 +1,7 @@
-import { View, Text, ScrollView, StyleSheet,Button } from 'react-native';
+import { View, Text, ScrollView, StyleSheet,Button, TouchableOpacity, Image, TextInput} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import post from '../components/post';
 import Post from '../components/post';
+
 
 export default function Home({navigation}){
 
@@ -9,15 +9,40 @@ export default function Home({navigation}){
         <>
             <View style = {styles.container}>
                 <View style = {styles.barraTopo}>
+                  <View style = {styles.usuario}>
 
+                    <Image source = {require("../assets/medalhas/medalhaBronze.png")} style={styles.medalha} />
+                    <Text style = {{fontWeight: 'bold', whiteSpace: 'nowrap', fontSize: '15px', display: 'flex',alignItems: 'center'}}>Caio Rangel</Text>
+                  </View>
+
+                  <View style = {{display: 'flex', width: '42%', alignItems: 'center', flexDirection: 'row',height: "60%", borderRadius: '10px', backgroundColor: 'white', marginLeft: '8%'}}>
+                    <TextInput style = {{width: '80%'}} />
+                    <Image source = {require("../assets/icones/iconeLupa.png")}
+                    style = {{width: '20px', height: '20px'}}/>
+                  </View>
+
+                  <Image source = {require("../assets/icones/menuLateral.png")}
+                  style = {{ width: '40px',height:'40px', marginLeft: '7%'}}/>
                 </View>
+
+                
                 <LinearGradient 
                 colors = {['#00AACC','#0066FF']}
                 style = {styles.tela}
                 >
+                  <View style = {styles.topoTela}>
+
+                  <Text style = {styles.titulo}>Física 3</Text>
+                  
+                  <TouchableOpacity style = {styles.criarTopico}>criar Tópico</TouchableOpacity>
+                  
+
+                  </View>
 
                   <Post></Post>  
-
+                  <Post></Post>  
+                  <Post></Post> 
+                  <Post></Post>  
 
                 </LinearGradient>
             </View>
@@ -29,19 +54,68 @@ export default function Home({navigation}){
 
 const styles = StyleSheet.create({
   container: {
-    
+    display: 'flex',
     width: '100%',
-    height: '100%',
+
+    alignItems: 'center',
   },
 
   barraTopo: {
-    backgroundColor: 'white',
+    backgroundColor: '#D9D9D9',
     width: '100%',
     height: '10%',
     boxShadow: '0px 0px 5px 0px black',
+    display:'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   tela: {
     width: '100%',
     height: '100%',
+    overflow: 'scroll',
+    display:'flex',
+    alignItems: 'center'
+  },
+  topoTela: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    opacity: '1',
+    marginBottom: '10%',
+    marginTop: '5%'
+  },
+  titulo: {
+    color: 'white',
+    fontSize: '25px',
+    fontWeight: '500',
+    marginLeft: '38%'
+  },
+  criarTopico: {
+
+    backgroundColor: 'white',
+    width: '25%',
+    height: '90%',
+    borderRadius: '20px',
+    fontSize: '15px',
+    fontWeight: '400',
+    display: 'flex',
+    textAlign:'center',
+    justifyContent: 'center',
+    whiteSpace: 'nowrap',
+    marginRight: '6%'
+  },
+  usuario:{
+    display:'flex',
+    flexDirection: 'row',
+    gap: '5%',
+   
+  },
+
+  medalha:{
+    width: '35px',
+    height: '40px',
+    objectFit: 'contain',
+    
   }
 });
--- components/post.js
+++ components/post.js
@@ -1,18 +1,91 @@
-import {Text,View} from 'react-native';
+import {Text,View, StyleSheet, Image} from 'react-native';
 
 
 function Post(props){
     return(
         <>
-        
-        <View style = {{width:'100px',height: '100px',backgroundColor: 'white'}}>
+          <View></View>
+            <View style = {styles.criarTopico}>
+              <View style = {styles.topo}>
+                
+                <View style = {styles.usuario}>
+                  <Image source = {require("../assets/medalhas/medalhaBronze.png")} style={styles.medalha} />
+                  <Text style = {{fontWeight: 'bold', whiteSpace: 'nowrap', fontSize: '10px', display: 'flex',alignItems: 'center', marginBottom: '5px'}}>Caio Rangel</Text>
+                </View>
 
-            <Text>Teste</Text>
+                <Text style = {{fontSize: '13px',color: 'black', opacity: '0.5'}}>Há duas horas</Text>
 
-        </View>
+                <Image source = {require("../assets/icones/iconeDenuncia.png")}
+                style = {{width:'20px',height: '20px',marginRight: '3px'}} />
+
+
+              </View>
+
+              <Text style = {{fontSize: '10px',fontWeight: 'bold',width: '75%',marginTop: '3%'}}
+              >Alguém poderia me enviar conteúdo sobre eletromagnetismo?? Estou desesperado 😭😭😭😭</Text>
+
+            </View>
+            <View style = {styles.respostas}>
+
+              <Image source = {require("../assets/icones/iconeComentario.png")}
+              style = {{width:'22px',height: '22px',marginTop: '3px',marginLeft: '3%'}}/>
+              <Text style = {{color: 'white',fontSize: "13px"}}>52</Text>
+
+            </View>
 
         </>
     )
 }
 
 export default Post;
+
+const styles = StyleSheet.create({
+  
+  criarTopico: {
+    marginTop: '10%',
+    width:'80%',
+    height: '16%',
+    backgroundColor: 'white',
+    display: 'flex',
+    flexDirection: "column",
+    borderTopRightRadius: "15px",
+    borderTopLeftRadius: '15px',
+    alignItems: 'center',
+  },
+  topo:{
+
+    width: '100%',
+    display: 'flex',
+    justifyContent:'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+
+  },
+
+   usuario:{
+    display:'flex',
+    flexDirection: 'row',
+    gap: '5%',
+   
+  },
+
+  medalha:{
+    width: '25px',
+    height: '30px',
+    objectFit: 'contain',
+    
+  },
+
+  respostas: {
+    height: '6%',
+    width: '80%',
+    backgroundColor: '#336699',
+    borderBottomRightRadius: '15px',
+    borderBottomLeftRadius: '15px',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: '3%',
+    
+  }
+});
