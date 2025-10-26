b07afcf1fb1ca51b63147b1c04eae8f7424d827f
MarquesDavi04 <gyava429@gmail.com>
2025-10-26 19:09:55 -0300

    teste

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
         <Stack.Screen name = "Login" component={Login}/>
         <Stack.Screen name = "Cadastro" component={Cadastro}/>
         <Stack.Screen name = "Home" component={Home}/>
