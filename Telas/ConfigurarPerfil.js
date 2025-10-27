3509dc0b7bf8ff7eb62cc9ea285fee9191a3dd95
MarquesDavi04 <gyava429@gmail.com>
2025-10-27 13:32:56 -0300

    inicio backend alterar cadastro + correçao das telas de home e perfil

--- Telas/ConfigurarPerfil.js
+++ Telas/ConfigurarPerfil.js
@@ -1,10 +1,36 @@
-import { View, Text, ScrollView, StyleSheet,Button, TouchableOpacity, Image, TextInput} from 'react-native';
+import { View, Text, ScrollView, StyleSheet,Button,Image, TextInput,TouchableOpacity, Touchable} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import Post from '../components/post';
+import { useState } from 'react'
+import { Feather } from '@expo/vector-icons';
+import { createClient } from '@supabase/supabase-js'
+
 
 
 export default function ConfigurarPerfil({navigation}){
-
+    const [nome, setNome] = useState('');
+    const [senha, setSenha] = useState('');
+    const [senha1, setSenha1] = useState('');
+    const [ocultarSenha, setOcultarSenha] = useState(true);
+    const [ocultarSenha1, setOcultarSenha1] = useState(true);
+    const supabaseUrl = 'https://uqwqhxadgzwrcarwuxmn.supabase.co/'
+    const supabaseKey = "sb_publishable_3wQ1GnLmKSFxOiAEzjVnsg_1EkoRyxV"
+    const supabase = createClient(supabaseUrl, supabaseKey)
+    async function handleInsert() {
+      if(senha == senha1 && senha.length >= 6){
+        const { data, error } = await supabase
+          .from('usuario')
+          .update({ senha: senha, nome: nome})
+          .eq('idusuario', '5')
+          .select()
+          
+          if (error) console.error(error)
+          else console.log('Usuário inserido:', data)
+      }else{
+        console.log('senhas não conferem')
+      }
+        
+    }
     return(
         <>
             <View style = {styles.container}>
@@ -12,39 +38,37 @@ export default function ConfigurarPerfil({navigation}){
                 colors = {['#00AACC','#0066FF']}
                 style = {styles.tela}
                 >
-                    <View style = {{width: '90%', marginLeft:'5%', marginVertical: '10%',gap:'50px', display:'flex',flexDirection: 'row', alignItems: 'center'}}>
-                        <TouchableOpacity style = {{width: '10%',backgroundColor: '#D9D9D9', borderRadius: '20px', paddingHorizontal: '30px', paddingVertical: '5px', display: 'flex', alignItems: 'center'}} onPress={() => navigation.navigate('Perfil')}>
+                    <View style = {{width: '90%', marginLeft:'5%', marginVertical: '10%',gap:50, display:'flex',flexDirection: 'row', alignItems: 'center'}}>
+                        <TouchableOpacity style = {{width: '10%',backgroundColor: '#D9D9D9', borderRadius: 20, paddingHorizontal: 30, paddingVertical: 5, display: 'flex', alignItems: 'center'}} onPress={() => navigation.navigate('Perfil')}>
                             <Image source = {require("../assets/icones/iconeSetaEsquerda.png")}
-                            style = {{width: '30px', height: '30px'}}/>
+                            style = {{width: 30, height: 30}}/>
                         </TouchableOpacity>
 
                         <Text style = {{fontSize: 20,fontWeight:'bold',color: 'white'}}>Configurações</Text>
                     </View>
 
-                    <View style = {styles.campo}>
-
-                        <Text style = {styles.textoCampo}>Nome:</Text>
-                        <TextInput style = {styles.inputCampo}/>
-
+                    <View style={{marginTop:"10%"}}>
+                      <Text style={{color:'#003366', marginLeft:"7%", marginTop:"5%", fontWeight: "500"}}>Nome Completo</Text>
+                      <TextInput value={nome} onChangeText={setNome} style={{width:"90%",backgroundColor: 'white', borderColor: 'gray', borderWidth: 1, paddingHorizontal: 10, borderRadius: 5, marginLeft:"5%"}}/>
+                      <Text style={{color:'#003366', marginLeft:"7%", marginTop:"5%", fontWeight: "500"}}>Nova Senha</Text>
+                      <View style={{flexDirection: 'row', alignItems:'center'}}>
+                        <TextInput style={{width:"90%",backgroundColor: 'white', borderColor: 'gray', borderWidth: 1, paddingHorizontal: 10, borderRadius: 5, marginLeft:"5%",paddingRight: 40}} secureTextEntry={ocultarSenha} value={senha} onChangeText={setSenha}/>
+                        <TouchableOpacity style={{position:'absolute', marginLeft:"85%"}} onPress={() => setOcultarSenha(!ocultarSenha)}>
+                          <Feather name={ocultarSenha ? 'eye-off' : 'eye'} size={22} color="#555" />
+                        </TouchableOpacity>
+                      </View>
+                      <Text style={{color:'#003366', marginLeft:"7%", marginTop:"5%", fontWeight: "500"}}>Confimar Nova Senha</Text>
+                      <View style={{flexDirection: 'row', alignItems:'center'}}>
+                        <TextInput style={{width:"90%",backgroundColor: 'white', borderColor: 'gray', borderWidth: 1, paddingHorizontal: 10, borderRadius: 5, marginLeft:"5%",paddingRight: 40}} secureTextEntry={ocultarSenha1} value={senha1} onChangeText={setSenha1}/>
+                        <TouchableOpacity style={{position:'absolute', marginLeft:"85%"}} onPress={() => setOcultarSenha1(!ocultarSenha1)}>
+                          <Feather name={ocultarSenha1 ? 'eye-off' : 'eye'} size={22} color="#555" />
+                        </TouchableOpacity>
+                      </View>
                     </View>
 
-                    <View style = {styles.campo}>
+                <View style = {{width: '100%',display: 'flex', justifyContent: 'center',alignItems: 'center',marginTop: 20}} >
 
-                        <Text style = {styles.textoCampo}>Nova senha:</Text>
-                        <TextInput style = {styles.inputCampo}/>
-
-                    </View>
-
-                    <View style = {styles.campo}>
-
-                        <Text style = {styles.textoCampo}>Confirmar senha:</Text>
-                        <TextInput style = {styles.inputCampo}/>
-
-                    </View>
-
-                <View style = {{width: '100%',display: 'flex', justifyContent: 'center',alignItems: 'center',marginTop: '20px'}} >
-
-                    <TouchableOpacity style = {styles.botao}>
+                    <TouchableOpacity style = {styles.botao} onPress={() => {handleInsert(); navigation.navigate('Perfil')}}>
                         <Text style = {{fontSize: 20,fontWeight: 'bold'}}>Salvar</Text>
                     </TouchableOpacity>
 
@@ -77,9 +101,10 @@ const styles = StyleSheet.create({
   campo: {
 
     display: 'flex',
+    alignItems:'center',
     flexDirection: 'column',
-    marginLeft: '15px',
-    gap:'10px',
+    marginLeft: 15,
+    gap:10,
     marginVertical: '5%',
   },
 
@@ -94,20 +119,20 @@ const styles = StyleSheet.create({
   inputCampo:{
 
     width: '70%',
-    height: '30px',
+    height: 30,
     backgroundColor: 'white',
-    borderRadius: '20px',
+    borderRadius: 20,
   },
 
   botao:{
 
     width: '50%',
-    height: '60px',
+    height: 60,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#D9D9D9',
-    borderRadius: '30px',
+    borderRadius: 30,
 
   },
 
--- Telas/perfil.js
+++ Telas/perfil.js
@@ -13,33 +13,30 @@ export default function Perfil({navigation}){
                 style = {styles.tela}
                 >
                    
-                   <Image source = {require("../assets/medalhas/medalhaBronze.png")}
-                   style = {{width: '45%',height: '37%'}} />
+                   <Image source = {require("../assets/medalhas/medalhaBronze.png")} style = {{width: '57%',height: '37%'}} />
                    <View style = {{display: 'flex',width: '80%', marginBottom: '5%', flexDirection: 'row',alignItems: 'center', justifyContent: 'space-between'}}>
                         <Text style = {{color: 'white', fontSize: 18, fontWeight: 'bold'}}>Caio Rangel</Text>
                         <TouchableOpacity onPress={() => navigation.navigate('ConfigurarPerfil')}>
 
                           <Image source={require("../assets/icones/iconeEngrenagem.png")}
-                          style = {{width: '45px',height: '45px'}}/>
+                          style = {{width: 45,height: 45}}/>
 
                         </TouchableOpacity>
                    </View>
-                    <View style = {{gap: '10px'}}>
-                        <Text style = {{color: 'white', fontSize: 15, fontWeight: '500'}}>Pontuação total do perfil: 500 </Text>
-                        <Text style = {{color: 'white', fontSize: 15, fontWeight: '500'}}>Pontuação necessária para a próxima medalha: 200</Text>
+                    <View style = {{gap: 10}}>
+                        <Text style = {{color: 'white', fontSize: 15, fontWeight: 500}}>Pontuação total do perfil: 500 </Text>
+                        <Text style = {{color: 'white', fontSize: 15, fontWeight: 500}}>Pontuação necessária para a próxima medalha: 200</Text>
                     </View>
 
-                    <View style = {{width: '100%',marginTop: '30px', color: 'black', display: 'flex', flexDirection: 'row',justifyContent: 'space-evenly'}}>
+                    <View style = {{width: '100%',marginTop: 30, color: 'black', display: 'flex', flexDirection: 'row',justifyContent: 'space-evenly'}}>
                       <Text style = {{fontSize: 20,fontWeight: 'bold',textDecorationLine: 'underline', color: 'white' }}>Dúvidas</Text>
                       <Text style = {{fontSize: 20,fontWeight: 'bold', color: 'white'}}>Respostas</Text>
                     </View>
-                    <View style = {{display: 'flex', alignItems: 'center'}}>
-                      
-                      <Post></Post>
-                      <Post></Post>
-                      <Post></Post>
 
-                    </View>
+                    <Post></Post>
+                    <Post></Post>
+                    <Post></Post>
+
                 </LinearGradient>
             </View>
             
